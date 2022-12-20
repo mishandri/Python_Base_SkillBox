bc7ada2464d3ec2edb2bec549ee4bcdd1a697c5b
mishandri <mishandri@gmail.com>
2022-12-20 16:55:19 +0300

    Выполнено задание 08 Урока 3

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -1,13 +1,25 @@
 # -*- coding: utf-8 -*-
 
 # (определение функций)
-import simple_draw
+import simple_draw as sd
 
 # Написать функцию отрисовки смайлика в произвольной точке экрана
 # Форма рожицы-смайлика на ваше усмотрение
-# Параметры функции: кордината X, координата Y, цвет.
+# Параметры функции: координата X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
+def smile(center_x, center_y):
+    """
+    Рисует смайлик
+    """
+    center = sd.get_point(center_x, center_y)
+    left = sd.get_point(center_x-50, center_y +50)
+    right = sd.get_point(center_x+50, center_y +50)
+    smile = sd.get_point(center_x, center_y - 100)
+    sd.circle(center, 200)
+    sd.circle(left, 10)
+    sd.circle(right, 10)
+    sd.circle(smile, 10)
 
-# TODO здесь ваш код
 
-simple_draw.pause()
+smile(300,300)
+sd.pause()
