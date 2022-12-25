8fbe40e120f095995a7887e4920a15fdede90829
mishandri <mishandri@gmail.com>
2022-12-25 12:29:09 +0300

    Решение задачи 1 Урока 5

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-
+import room_1
+import room_2
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
-
+print("В комнате room_1 живут:", room_1.folks)
+print("В комнате room_2 живут:", room_2.folks)
 
