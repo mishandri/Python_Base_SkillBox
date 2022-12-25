0378aef9e7e8bd37705dae81902cb1e14e4b37b9
mishandri <mishandri@gmail.com>
2022-12-25 13:44:41 +0300

    Решение задачи 2 Урока 5

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -6,6 +6,21 @@
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
 # TODO здесь ваш код
+import district.central_street.house1.room1 as csh1r1
+import district.central_street.house1.room2 as csh1r2
+import district.central_street.house2.room2 as csh2r1
+import district.central_street.house2.room2 as csh2r2
+import district.soviet_street.house1.room1 as ssh1r1
+import district.soviet_street.house1.room2 as ssh1r2
+import district.soviet_street.house2.room1 as ssh2r1
+import district.soviet_street.house2.room2 as ssh2r2
+
+raion = []
+raion.extend(csh1r1.folks + csh1r2.folks + csh2r1.folks + csh2r2.folks +
+             ssh1r1.folks + ssh1r2.folks + ssh2r1.folks + ssh2r2.folks)
+
+print(', '.join(raion))
+
 
 
 
