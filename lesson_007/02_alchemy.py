f79eebd5f05b58eda579caa7e9544d2aad1bda49
mishandri <mishandri@gmail.com>
2023-01-05 15:11:21 +0300

    Задание 2 Урок 7. Выполнена основная часть задания

--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -3,6 +3,87 @@
 # Создать прототип игры Алхимия: при соединении двух элементов получается новый.
 # Реализовать следующие элементы: Вода, Воздух, Огонь, Земля, Шторм, Пар, Грязь, Молния, Пыль, Лава.
 # Каждый элемент организовать как отдельный класс.
+
+class Water:
+    def __str__(self):
+        return 'Вода'
+
+    def __add__(self, other):
+        if type(other) is Air:
+            return Storm()
+        elif type(other) is Fire:
+            return Steam()
+        elif type(other) is Air:
+            return Storm()
+        else:
+            return None
+
+
+class Air:
+    def __str__(self):
+        return 'Воздух'
+
+    def __add__(self, other):
+        if type(other) is Water:
+            return Storm()
+        elif type(other) is Fire:
+            return Flash()
+        elif type(other) is Earth:
+            return Dust()
+        else:
+            return None
+
+
+class Fire:
+    def __str__(self):
+        return 'Огонь'
+
+    def __add__(self, other):
+        if type(other) is Water:
+            return Steam()
+        elif type(other) is Air:
+            return Flash()
+        elif type(other) is Earth:
+            return Lava()
+        else:
+            return None
+
+
+class Earth:
+    def __str__(self):
+        return 'Земля'
+
+
+class Storm:
+    def __str__(self):
+        return 'Шторм'
+
+
+class Steam:
+    def __str__(self):
+        return 'Пар'
+
+
+class Dirt:
+    def __str__(self):
+        return 'Грязь'
+
+
+class Flash:
+    def __str__(self):
+        return 'Молния'
+
+
+class Dust:
+    def __str__(self):
+        return 'Пыль'
+
+
+class Lava:
+    def __str__(self):
+        return 'Лава'
+
+
 # Таблица преобразований:
 #   Вода + Воздух = Шторм
 #   Вода + Огонь = Пар
@@ -11,6 +92,8 @@
 #   Воздух + Земля = Пыль
 #   Огонь + Земля = Лава
 
+print(Water(), '+', Air(), '=', Water() + Air())
+print(Fire(), '+', Air(), '=', Fire() + Air())
 # Сложение элементов реализовывать через __add__
 # Если результат не определен - то возвращать None
 # Вывод элемента на консоль реализовывать через __str__
