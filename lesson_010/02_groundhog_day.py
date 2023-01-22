65d0da956466546e5770580883fc902df2c0d77f
mishandri <mishandri@gmail.com>
2023-01-22 16:32:07 +0300

    Выполнено Задание 2 Урока 10

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -16,8 +16,71 @@
 # кармы до уровня ENLIGHTENMENT_CARMA_LEVEL. Исключения обработать и записать в лог.
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
+from random import randint
 
-ENLIGHTENMENT_CARMA_LEVEL = 777
+
+class IamGodError(Exception):
+    pass
+
+
+class DrunkError(Exception):
+    pass
+
+
+class CarCrashError(Exception):
+    pass
+
+
+class GluttonyError(Exception):
+    pass
+
+
+class DepressionError(Exception):
+    pass
+
+
+class SuicideError(Exception):
+    pass
+
+
+def one_day():
+    carma = randint(1, 7)
+    end_day = randint(1, 13)
+    if end_day == 1:
+        raise IamGodError('Почувствовал себя Богом')
+    elif end_day == 2:
+        raise DrunkError('Напился до потери пульса')
+    elif end_day == 3:
+        raise CarCrashError('Погиб в автокатастрофе')
+    elif end_day == 4:
+        raise GluttonyError('Лопнул живот от обжорства')
+    elif end_day == 5:
+        raise DepressionError('Умер от депрессии')
+    elif end_day == 6:
+        raise SuicideError('Решил закончить это всё')
+    else:
+        return carma
+
+ENLIGHTENMENT_CARMA_LEVEL = 0
+day = 0
+
+file_log = '02_log.txt'
+
+while ENLIGHTENMENT_CARMA_LEVEL <= 777:
+    day += 1
+    try:
+        carma = one_day()
+        ENLIGHTENMENT_CARMA_LEVEL += carma
+        with open(file_log, 'a', encoding='utf8') as file:
+            file.write(f'День {day}. КАРМА = {ENLIGHTENMENT_CARMA_LEVEL}\n')
+    except Exception as exc:
+        ENLIGHTENMENT_CARMA_LEVEL -= 1
+        with open(file_log, 'a', encoding='utf8') as file:
+            file.write(f'День {day}. {exc}. КАРМА = {ENLIGHTENMENT_CARMA_LEVEL}\n')
+else:
+    print('Наконец-то этот день закончился!')
+
+#
 
 # TODO здесь ваш код
 
