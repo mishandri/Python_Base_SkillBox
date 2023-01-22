5c01c675e48384019db69de434967e96e8e54c0d
mishandri <mishandri@gmail.com>
2023-01-22 11:08:58 +0300

    Задание 3: Выполнено

--- lesson_009/03_files_arrange.py
+++ lesson_009/03_files_arrange.py
@@ -35,6 +35,29 @@ import os, time, shutil
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
 # TODO здесь ваш код
+class HandlerFiles:
+    in_dir = ''
+    out_dir = ''
+    def __init__(self, in_dir, out_dir):
+        self.in_dir = os.path.normpath(in_dir)
+        self.out_dir = os.path.normpath(out_dir)
+
+    def handle(self):
+        for dirpath, dirnames, filenames in os.walk(self.in_dir):
+            for file in filenames:
+                full_file_path = os.path.join(dirpath, file)
+                secs = os.path.getmtime(full_file_path)
+                file_time = time.gmtime(secs)
+                full_out_file_path = os.path.join(self.out_dir, f'{file_time[0]}', f'{file_time[1]:0>2}', file)
+                os.makedirs(os.path.join(self.out_dir, f'{file_time[0]}', f'{file_time[1]:0>2}'), exist_ok=True)
+                shutil.copy2(full_file_path, full_out_file_path)
+                # print(full_file_path)
+                # print(full_out_file_path)
+
+
+hf = HandlerFiles(f'icons', f'icons_by_year')
+hf.handle()
+
 
 # Усложненное задание (делать по желанию)
 # Нужно обрабатывать zip-файл, содержащий фотографии, без предварительного извлечения файлов в папку.
