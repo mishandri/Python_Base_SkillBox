1e590b5810be9ffa0ec686960d8491dab01860f4
kolcharma <kolcharma@gmail.com>
2023-01-30 17:51:18 +0300

    Урок 11, задание 3. Выполнена часть 3.1

--- lesson_011/02_prime_numbers_part3.py
+++ lesson_011/02_prime_numbers_part3.py
@@ -5,6 +5,20 @@
 #       то для вычисления "счастливости" брать равное количество цифр с начала и конца:
 #           727 -> 7(2)7 -> 7 == 7 -> True
 #           92083 -> 92(0)83 -> 9+2 == 8+3 -> True
+numbers = [727, 92083, 55556, 897456, 5467897]
+def happy_number(n):
+    n = str(n)
+    left = 0
+    for i in n[0:len(n)//2]:
+        left += int(i)
+    right = 0
+    for i in n[(len(n) + 1) // 2:]:
+        right += int(i)
+    return True if left == right else False
+
+filter_numbers = filter(happy_number, numbers)
+print(list(filter_numbers))
+
 # 2) "палиндромное" - одинаково читающееся в обоих направлениях. Например 723327 и 101
 # 3) придумать свою (https://clck.ru/GB5Fc в помощь)
 #
